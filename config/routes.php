5f845dc5dfb836d19a34e95db628db924ad119a8
Izocel <68454661+Izocel@users.noreply.github.com>
2022-02-04 09:40:42 -0500

    final route group for user CRUD

--- config/routes.php
+++ config/routes.php
@@ -9,30 +9,28 @@ use Slim\Psr7\Request;
 use Slim\Psr7\Response;
 
 return function (App $app) {
+
+    // Messsage de bienvenue
     $app->get('/', \App\Action\HomeAction::class)->setName('home');
+
+    // Documentation de l'api
+    $app->get('/docs', \App\Action\Docs\SwaggerUiAction::class);
+
+    // Insertion d'un usager
+    $app->post('/users/', \App\Action\UserCreateAction::class);
 
 
     /**
-     * $app->get('/users', \App\Action\UserCreateAction::class);
-     * GET	/users/{id}	Lister seulement l'usager avec le id en paramètre
-     * PUT	/users/{id}	Modifier l'usager avec le id en paramètre
+     * GET	    /users/{id}	Lister seulement l'usager avec le id en paramètre
+     * PUT	    /users/{id}	Modifier l'usager avec le id en paramètre
      * DELETE	/users/{id}	Supprimer l'usager avec le id en paramètre
      */
      $app->group('/users/{id:[0-9]+}', function (RouteCollectorProxy $group) {
          $group->get('', \App\Action\UserReadAction::class);
-         $group->put('', \App\Action\UserReadAction::class);
-
-         $group->delete('', function(Request $request, Response $response, array $arguments): Response {
-
-            return $response;
-         });
+         $group->put('', \App\Action\UserUpdateAction::class);
+         $group->delete('', \App\Action\UserUpdateAction::class);
      });
 
-
-    $app->post('/users/', \App\Action\UserCreateAction::class);
-
-    // Documentation de l'api
-    $app->get('/docs', \App\Action\Docs\SwaggerUiAction::class);
 
 };
 

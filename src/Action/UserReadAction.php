fc2644228194161f1ef487ccf675808b34d90a82
Izocel <triangle1232009@hotmail.com>
2022-02-11 14:06:23 -0500

    refacto en cours pour  Action-Ctrl-Repository

--- src/Action/UserReadAction.php
+++ src/Action/UserReadAction.php
@@ -3,6 +3,7 @@
 namespace App\Action;
 
 use App\Domain\User\Service\UserReader;
+use App\Factory\LoggerFactory;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Slim\Psr7\Response;
@@ -10,48 +11,59 @@ use Slim\Psr7\Response;
 final class UserReadAction
 {
     private $userReader;
+    private $logger;
+    private $data;
 
-    public function __construct(UserReader $userReader)
+    public function __construct(UserReader $userReader, LoggerFactory $loggerFactory)
     {
+        $this->logger = $loggerFactory
+        ->addFileHandler('test.log')
+        ->createLogger("LoggerTest");
+
         $this->userReader = $userReader;
     }
 
     public function __invoke(
-        ServerRequestInterface $request, 
+        ServerRequestInterface $request,
         ResponseInterface $response
-    ): ResponseInterface {  
+    ): ResponseInterface {
+
+        //TODO: Remplacer $data par un constructeur ou un factory
         // Collecte les données à partir de la requête HTTP
-        $data = (object)$request->getAttributes();
+        $attribs = (object)$request->getAttributes() ?? null;
+        $params = (object)$request->getQueryParams() ?? null;
+        $body = (object)$request->getParsedBody() ?? null;
+        $this->data = (object)["attribs"=>$attribs,"params"=>$params,"body"=>$body];
 
-        // Invoque le Domaine avec les données en entrée et retourne le résultat
-        $resultat = $this->userReader->selectUser($data->id ?? 0);
 
+        $resultat = $this->userReader->selectUser($this->data);
         return $this->respondWithFormat($resultat, $response);
     }
 
 
     /**
-     * @param array $data Les données qui seront utilisées pour la réponse
+     * @param array $atts Les données qui seront utilisées pour la réponse
      * 
      * @param Response $response l'objet réponse pour la requête en cours
      * 
      * @return Response $response La réponse fromaté selon l'algorithme
      */
-    private function respondWithFormat(array $data, Response $response): Response {
-        
-        $errors = $data['errors'] ? true : false;
-        $notFoundErrors = $data['notFound-errors'] ? true : false;
-        $validationErrors = $data['validation-errors'] ? true : false;
+    private function respondWithFormat(array $atts, Response $response): Response
+    {
 
-        
+        $errors = $atts['errors'] ? true : false;
+        $notFoundErrors = $atts['notFound-errors'] ? true : false;
+        $validationErrors = $atts['validation-errors'] ? true : false;
+
+
 
         // Envoit les résultats dans le body de la réponse
-        $response->getBody()->write((string)json_encode($data));
+        $response->getBody()->write((string)json_encode($atts));
 
-        if($notFoundErrors){
+        if ($notFoundErrors) {
             return $response
-            ->withHeader('Content-Type', 'application/json')
-            ->withStatus(404);
+                ->withHeader('Content-Type', 'application/json')
+                ->withStatus(404);
         }
 
         return $response
